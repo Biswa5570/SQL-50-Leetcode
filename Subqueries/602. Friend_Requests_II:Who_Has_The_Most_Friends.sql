ea1b1db971cbd5c6cecb49cf301427b733bf80c6
Biswamohan singh <prasannasingh50@gmail.com>
2024-11-21 18:09:48 +0530

    Create 602. Friend_Requests_II_:_Who_Has_The_Most_Friends.sql

---
+++ Subqueries/602. Friend_Requests_II:Who_Has_The_Most_Friends.sql
@@ -0,0 +1,21 @@
+SELECT requester_id AS id, 
+    ( SELECT COUNT(*) 
+    FROM RequestAccepted 
+    WHERE requester_id = id OR accepter_id = id
+    ) AS num
+    
+FROM RequestAccepted 
+GROUP BY requester_id
+
+UNION
+
+SELECT accepter_id as id,
+    ( SELECT COUNT(*) 
+    FROM RequestAccepted  
+    WHERE requester_id = id OR accepter_id = id
+    ) AS num
+    
+FROM RequestAccepted
+GROUP BY accepter_id
+
+ORDER BY num DESC LIMIT 1
